fbb4137dee8b1df653ee4d2609fe704b816acce3
Maximiliano <maxikotvi@gmail.com>
2017-03-13 12:23:54 -0300

    Initial set up: project created, pg installed (and tested)

---
+++ twitter_project/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterProject::Application.config.secret_key_base = '39290199891fb8019888c174459c3766a40b20324af6bf56ff95d25207a12b8eb7c9b7e7d99ad1d25e582292819fed7eb171b57d94d6776eb4ef53072519e2bc'
