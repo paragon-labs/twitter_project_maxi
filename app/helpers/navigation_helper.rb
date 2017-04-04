1a22642fb19a9d9c6dc5ca9981c8ba13daa385d1
Maximiliano <maxikotvi@gmail.com>
2017-04-04 14:33:49 -0300

    Left navigation pane added

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -4,11 +4,20 @@ module NavigationHelper
     render partial: 'layouts/navigation', locals: { current_tab: tab }
   end
 
-  def top_nav_tab(title, path, current_tab)
-    title == current_tab ? active_class = 'active' : active_class = ''
+  def currently_at_left(tab, sub_tab = nil)
+    render partial: 'layouts/left_navigation', locals: { current_tab: tab, current_sub_tab: sub_tab }
+  end
+
+  def nav_tab(title, path, current_tab)
+    active_class = title == current_tab ? 'active' : ''
     content_tag :li, role: 'presentation', class: active_class do
       link_to title, path
     end
+  end
+
+  def left_tab_sub_tabs(tab, sub_tab)
+    path = tab == 'Admin' ? 'admin' : 'home'
+    render partial: "#{path}/left_nav", locals: { current_sub_tab: sub_tab }
   end
 
 end
