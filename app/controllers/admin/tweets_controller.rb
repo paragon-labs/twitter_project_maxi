770b3c03f85b9fc7d99827971a113bbb80d4b8fa
Maximiliano <maxikotvi@gmail.com>
2017-04-18 12:52:47 -0300

    Basic Create

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -7,5 +7,26 @@ module Admin
       @tweets = Tweet.all
     end
 
+    def new
+      @tweet = Tweet.new
+    end
+
+    def create
+      @tweet = Tweet.new(tweet_params)
+      if @tweet.save
+        flash[:notice] = 'Tweet created successfully.'
+        redirect_to admin_tweets_path
+      else
+        render 'new'
+      end
+    end
+
+    private
+
+    def tweet_params
+      params.require(:tweet).permit(:number, :text, :user_name, :user_screen_name,
+                                    :user_url, :user_profile_img)
+    end
+
   end
 end
--- spec/controllers/admin/tweets_controller_spec.rb
+++ spec/controllers/admin/tweets_controller_spec.rb
@@ -28,4 +28,29 @@ RSpec.describe Admin::TweetsController, type: :controller do
 
   end
 
+  describe 'POST #create' do
+
+    let(:tweet) { build(:tweet) }
+
+    context 'when user is admin' do
+      it 'creates a new tweet' do
+        sign_in admin
+
+        post :create, tweet: { number: tweet.number, text: 'SomeText' }
+
+        expect(response).to redirect_to admin_tweets_path
+      end
+    end
+    context 'when user is not an admin' do
+      it 'redirects to the home page' do
+        sign_in user
+
+        post :create, tweet: { number: tweet.number, text: 'SomeText' }
+
+        expect(response).to redirect_to tweets_path
+      end
+    end
+
+  end
+
 end
